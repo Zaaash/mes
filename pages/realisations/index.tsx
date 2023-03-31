f23084c8f62ee66bf71ae8ee1d32069ff30d4b4a
Stéphane DesPLACES <sdesplaces.atos@lfdj.com>
2023-03-31 11:35:42 +0200

    reals

--- pages/realisations/index.tsx
+++ pages/realisations/index.tsx
@@ -30,18 +30,180 @@ const txts: any = {
       title: "Galerie photos",
       subTitle: "Survolez à votre guise toutes les facettes de notre savoir faire et cliquez pour en savoir plus...",
       reals: [
-        { id: "cardio", name: "Centre cardio-thoracique de Monaco", im: imgs.cardio },
-        { id: "cardio", name: "Bureaux Scorpio", im: imgs.scorpio },
-        { id: "cardio", name: "Société de Bains de Mer", im: imgs.sbm },
-        { id: "cardio", name: "Centre hospitalier Princesse Grâce", im: imgs.r4 },
-        { id: "cardio", name: "Tour Les Giroflées", im: imgs.giroflees },
-        { id: "cardio", name: "Stade Louis II", im: imgs.stade },
-        { id: "cardio", name: "Hôtel de Paris", im: imgs.hotelParis },
-        { id: "cardio", name: "Yacht Club de Monaco", im: imgs.yachtClub },
-        { id: "cardio", name: "Grimaldi Forum", im: imgs.r5 },
-        { id: "cardio", name: "Mareterra", im: imgs.mareterra },
-        { id: "cardio", name: "Hôtel Le Couvent", im: imgs.r7 },
-        { id: "cardio", name: "La réserve Ramatuelle", im: imgs.r10 },
+        {
+          id: "scorpio-m",
+          im: [imgs.scorpio1, imgs.scorpio2, imgs.scorpio3],
+          ref: "mes",
+          name: "Bureaux entreprise Scorpio",
+          descr: "2000m² de bureaux sur 2 niveaux. Réalisation des lots CFA et CFO.",
+        },
+        {
+          id: "cafeParis-m",
+          im: [imgs.cafeParis1, imgs.cafeParis2],
+          ref: "mes",
+          name: "Café de Paris (SBM)",
+          descr: "Rénovation et surélévation. Réalisation des lots CFA et CFO.",
+        },
+        {
+          id: "stade",
+          im: [imgs.stade1],
+          ref: "mes",
+          name: "Stade Louis II",
+          descr:
+            "Opération de prestige. Modernisation de l’éclairage scénique avec 380 projecteurs à leds entièrement pilotés par ordinateur",
+        },
+        {
+          id: "hotelParis-m",
+          im: [imgs.hotelParis1, imgs.hotelParis2],
+          ref: "mes",
+          name: "Hôtel de Paris (SBM)",
+          descr: "Rénovation et surélévation. Réalisation des lots CFA et CFO.",
+        },
+        {
+          id: "mareterra-f",
+          im: [imgs.mareterra1, imgs.mareterra2, imgs.mareterra3],
+          ref: "mes",
+          name: "Opération Mareterra",
+          descr: "Extension du Grimaldi Forum. 6000m² de salle d’exposition modulaire. Réalisation des lots CFA CFO.",
+        },
+        {
+          id: "mareterra-l",
+          im: [imgs.mareterra1, imgs.mareterra2, imgs.mareterra3],
+          ref: "mes",
+          name: "Opération Mareterra",
+          descr:
+            "Eclairage extérieur du site. 100 mâts équipés de projecteur led. 790 ml de cordon led sur la promenade du bord de mer.",
+        },
+        {
+          id: "ilotPasteur",
+          im: [imgs.ilotPasteur1, imgs.ilotPasteur2],
+          ref: "mes",
+          name: "Ilôt Pasteur",
+          descr: "Construction d’un bâtiment multimodal. Réalisation des lots CFA et CFO.",
+        },
+        {
+          id: "giroflees",
+          im: [imgs.giroflees1],
+          ref: "mes",
+          name: "Tour Les Giroflées",
+          descr: "Construction d'une tour de logements de luxe. Réalisation des lots CFA et CFO.",
+        },
+        {
+          id: "palais",
+          im: [imgs.palais1],
+          ref: "mes",
+          name: "Palais Princier",
+          descr: "Mise en lumière des façades.",
+        },
+        {
+          id: "scorpio-c",
+          im: [imgs.scorpio2, imgs.scorpio1],
+          ref: "c2s",
+          name: "Bureaux entreprise Scorpio",
+          descr: "2000m² de bureaux sur 2 niveaux. Réalisation des lots CVC et Plomberie.",
+        },
+        {
+          id: "cafeParis-c",
+          im: [imgs.cafeParis2, imgs.cafeParis1],
+          ref: "c2s",
+          name: "Café de Paris (SBM)",
+          descr: "Rénovation et surélévation. Réalisation des lots CVC et Plomberie.",
+        },
+        {
+          id: "nchpg-m",
+          im: [imgs.nchpg1, imgs.nchpg2],
+          ref: "c2s",
+          name: "Nouveau Centre Hospitalier Princesse Grâce",
+          descr: "Service des travaux publics construction. Réalisation de la production du Lot CVC.",
+        },
+        {
+          id: "hotelParis-c",
+          im: [imgs.hotelParis2, imgs.hotelParis1],
+          ref: "c2s",
+          name: "Hôtel de Paris (SBM)",
+          descr: "Rénovation et surélévation. Réalisation des lots CVC et Plomberie.",
+        },
+        {
+          id: "mareterra-p",
+          im: [imgs.mareterra3, imgs.mareterra1, imgs.mareterra2],
+          ref: "c2s",
+          name: "Opération Mareterra",
+          descr:
+            "Extension du Grimaldi Forum. 6000m² de salle d’exposition modulaire. Réalisation des lots CVC et Plomberie.",
+        },
+        {
+          id: "seaWergie",
+          im: [imgs.seaWergie1, imgs.seaWergie2],
+          ref: "c2s",
+          name: "Sea Wergie",
+          descr: "Unité principale de production du site du Larvotto.",
+        },
+        {
+          id: "ilotPasteur",
+          im: [imgs.ilotPasteur2, imgs.ilotPasteur1],
+          ref: "c2s",
+          name: "Ilôt Pasteur",
+          descr: "Construction d’un bâtiment multimodal. Réalisation des lots CVC et Plomberie.",
+        },
+        {
+          id: "giroflees",
+          im: [imgs.giroflees1],
+          ref: "c2s",
+          name: "Tour Les Giroflées",
+          descr: "Construction d'une tour de logements de luxe. Réalisation des lots CVC et Plomberie.",
+        },
+        {
+          id: "cardio",
+          im: [imgs.cardio1],
+          ref: "i2s",
+          name: "Centre cardio-thoracique de Monaco",
+          descr: "Solution de télécommunications unifiées.",
+        },
+        {
+          id: "chpg",
+          im: [imgs.r4],
+          ref: "i2s",
+          name: "Centre hospitalier Princesse Grâce",
+          descr: "Contrôle d’accès et vidéoprotection en architecture ANSSI.",
+        },
+        {
+          id: "grimaldiForum",
+          im: [imgs.r5],
+          ref: "i2s",
+          name: "Grimaldi Forum",
+          descr: "Contrôle d’accès et vidéoprotection en architecture ANSSI.",
+        },
+        {
+          id: "edenRoc",
+          im: [imgs.edenRoc1],
+          ref: "i2s",
+          name: "Hôtel du Cap Eden Roc",
+          descr:
+            "Solution de Communications Unifiées Multisites. Infrastructure Wifi haute densité. Lan Haut Débit. IP/TV. Contrôle d’accès, vidéoprotection.",
+        },
+        {
+          id: "couvent",
+          im: [imgs.couvent1],
+          ref: "i2s",
+          name: "Hôtel Le Couvent",
+          descr:
+            "Solution de Communications Unifiées. Infrastructure Wifi haute densité. Lan Haut Débit / Firewall. Contrôle d’accès, vidéoprotection.",
+        },
+        {
+          id: "reserve",
+          im: [imgs.r10],
+          ref: "i2s",
+          name: "La réserve Ramatuelle",
+          descr: "Solution de Communications Unifiées. Infrastructure Wifi haute densité.",
+        },
+        {
+          id: "yachtClub",
+          im: [imgs.yachtClub1],
+          ref: "i2s",
+          name: "Yacht Club de Monaco",
+          descr:
+            "Solution de Communications Unifiées. Contrôle d’accès, vidéoprotection. Lan haut débit / Firewall. Wifi haute densité.",
+        },
       ],
     },
   },
@@ -149,25 +311,21 @@ const Realisations: NextPage = () => {
                   className={styles.real}
                 >
                   <Image
-                    src={real.im}
+                    src={real.im[0]}
                     alt=""
                   />
 
                   <div>
                     <strong>{real.name}</strong>
 
-                    <span onClick={() => handleZoom(real.im, real.id)}>
+                    <span>{real.descr}</span>
+
+                    <i onClick={() => handleZoom(real.im, real.id)}>
                       <FontAwesomeIcon
                         icon={pictosSol.faPhotoVideo}
                         title="Zoom photo"
                       />
-                    </span>
-                    <Link href={`/realisations/detail?id=${real.id}`}>
-                      <FontAwesomeIcon
-                        icon={pictosSol.faLink}
-                        title="Voir la fiche descriptive du projet"
-                      />
-                    </Link>
+                    </i>
                   </div>
                 </div>
               )
--- utils/helpers/images.ts
+++ utils/helpers/images.ts
@@ -4,16 +4,23 @@ import actu3 from "../../assets/images/actu-extension-data-center.webp"
 import actu4 from "../../assets/images/actu-extension-puissance-centrale-fontvieille.webp"
 import brevet from "../../assets/images/fournisseur-brevete-du-prince-monaco.webp"
 import c2sAbout from "../../assets/images/c2s-about.webp"
-import cardio from "../../assets/images/centre-cardio-thoracique-monaco.webp"
+import cafeParis1 from "../../assets/images/real-cafe-Paris-1.webp"
+import cafeParis2 from "../../assets/images/real-cafe-Paris-2.webp"
+import cardio1 from "../../assets/images/real-centre-cardio-thoracique-monaco-1.webp"
+import couvent1 from "../../assets/images/real-hotel-couvent-1.webp"
+import edenRoc1 from "../../assets/images/real-hotel-cap-eden-roc-1.webp"
 import engagementEngage from "../../assets/images/engagement-engage.webp"
 import engagementRecrute from "../../assets/images/engagement-recrute.webp"
-import giroflees from "../../assets/images/real-giroflees.webp"
-import hotelParis from "../../assets/images/real-hotelParis.webp"
+import giroflees1 from "../../assets/images/real-tour-giroflees-1.webp"
+import hotelParis1 from "../../assets/images/real-hotel-Paris-1.webp"
+import hotelParis2 from "../../assets/images/real-hotel-Paris-2.webp"
 import hpC2s from "../../assets/images/c2s-hp.webp"
 import hpI2s from "../../assets/images/i2s-hp.webp"
 import hpMes from "../../assets/images/mes-hp.webp"
 import i2sAbout from "../../assets/images/i2s-about.webp"
 import i2sValues from "../../assets/images/i2s-values.webp"
+import ilotPasteur1 from "../../assets/images/real-ilot-Pasteur-1.webp"
+import ilotPasteur2 from "../../assets/images/real-ilot-Pasteur-2.webp"
 import intro from "../../assets/images/Monaco-de-nuit.png"
 import introEngagement from "../../assets/images/intro-engagement.webp"
 import introMES from "../../assets/images/intro-mes.webp"
@@ -29,10 +36,15 @@ import logoList from "../../assets/images/logos.webp"
 import logoMes from "../../assets/images/logo-MES.webp"
 import logoPalaisPrincier from "../../assets/images/logo-palaisPrincier.webp"
 import logoSBM from "../../assets/images/logo-SBM.webp"
-import mareterra from "../../assets/images/real-mareterra.webp"
+import mareterra1 from "../../assets/images/real-mareterra-1.webp"
+import mareterra2 from "../../assets/images/real-mareterra-2.webp"
+import mareterra3 from "../../assets/images/real-mareterra-3.webp"
 import mesServices from "../../assets/images/mes-services.webp"
+import nchpg1 from "../../assets/images/real-nouveau-centre-hospitalier-Princesse-Grace-1.webp"
+import nchpg2 from "../../assets/images/real-nouveau-centre-hospitalier-Princesse-Grace-2.webp"
 import offreCompetences from "../../assets/images/offre-competences.webp"
 import offreServices from "../../assets/images/offre-services.webp"
+import palais1 from "../../assets/images/real-palais-princier-1.webp"
 import pictoAcces from "../../assets/images/picto-acces.webp"
 import pictoChauffClim from "../../assets/images/picto-chauffClim.webp"
 import pictoConseil from "../../assets/images/picto-conseil.webp"
@@ -54,13 +66,15 @@ import r3 from "../../assets/images/r3.webp"
 import r4 from "../../assets/images/r4.webp"
 import r5 from "../../assets/images/r5.webp"
 import r6 from "../../assets/images/r6.webp"
-import r7 from "../../assets/images/r7.webp"
 import r8 from "../../assets/images/r8.webp"
 import r9 from "../../assets/images/r9.webp"
-import sbm from "../../assets/images/real-sbm.webp"
-import scorpio from "../../assets/images/real-scorpio.webp"
-import stade from "../../assets/images/real-stade.webp"
-import yachtClub from "../../assets/images/real-yachtClub.webp"
+import scorpio1 from "../../assets/images/real-bureaux-scorpio-1.webp"
+import scorpio2 from "../../assets/images/real-bureaux-scorpio-2.webp"
+import scorpio3 from "../../assets/images/real-bureaux-scorpio-3.webp"
+import seaWergie1 from "../../assets/images/real-sea-wergie-1.webp"
+import seaWergie2 from "../../assets/images/real-sea-wergie-2.webp"
+import stade1 from "../../assets/images/real-stade-louis-II-1.webp"
+import yachtClub1 from "../../assets/images/real-yacht-club-1.webp"
 
 export {
   actu1,
@@ -69,7 +83,11 @@ export {
   actu4,
   brevet,
   c2sAbout,
-  cardio,
+  cafeParis1,
+  cafeParis2,
+  cardio1,
+  couvent1,
+  edenRoc1,
   engagementEngage,
   engagementRecrute,
   i2sAbout,
@@ -78,9 +96,13 @@ export {
   introEngagement,
   introMES,
   introOffre,
+  hotelParis1,
+  hotelParis2,
   hpMes,
   hpI2s,
   hpC2s,
+  ilotPasteur1,
+  ilotPasteur2,
   logoMes,
   logoI2s,
   logoCCM,
@@ -92,9 +114,13 @@ export {
   logoInforca,
   logoPalaisPrincier,
   logoSBM,
+  mareterra1,
+  mareterra2,
+  mareterra3,
   mesServices,
   offreCompetences,
   offreServices,
+  palais1,
   pictoTel,
   pictoWifi,
   pictoAcces,
@@ -113,17 +139,19 @@ export {
   r4,
   r5,
   r6,
-  r7,
   r8,
   r9,
   r10,
   r11,
   r12,
-  giroflees,
-  hotelParis,
-  mareterra,
-  sbm,
-  scorpio,
-  stade,
-  yachtClub,
+  giroflees1,
+  nchpg1,
+  nchpg2,
+  scorpio1,
+  scorpio2,
+  scorpio3,
+  seaWergie1,
+  seaWergie2,
+  stade1,
+  yachtClub1,
 }
